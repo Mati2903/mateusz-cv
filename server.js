17b2a60b61c1a5b92d252ecd191e3fea8e37b0c6
Mati2903 <mazurek321.dev@gmail.com>
2022-03-20 22:42:23 +0100

    change token oauth

--- server.js
+++ server.js
@@ -4,80 +4,81 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
-const {google} = require("googleapis");
+const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
-const smtpTransport = require('nodemailer-smtp-transport');
+const smtpTransport = require("nodemailer-smtp-transport");
 
 app.use(express.json());
 app.use(cors());
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      req.sendFile(path.resolve(__dirname, "build", "index.html"));
-    })
-  }
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("build"));
+	app.get("*", (req, res) => {
+		req.sendFile(path.resolve(__dirname, "build", "index.html"));
+	});
+}
 
-  const oauth2Client = new OAuth2(
-    process.env.OAUTH_CLIENTID,
-    process.env.OAUTH_CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
+const oauth2Client = new OAuth2(
+	process.env.OAUTH_CLIENTID,
+	process.env.OAUTH_CLIENT_SECRET,
+	"https://developers.google.com/oauthplayground"
 );
 
-  oauth2Client.setCredentials({
-    refresh_token: process.env.OAUTH_REFRESH_TOKEN
+oauth2Client.setCredentials({
+	refresh_token: process.env.OAUTH_REFRESH_TOKEN,
 });
-const accessToken = oauth2Client.getAccessToken()
+const accessToken = oauth2Client.getAccessToken();
 
-let transporter = nodemailer.createTransport(smtpTransport({
- service: "gmail",
- 
- auth: {
-   type: "OAuth2",
-   user: process.env.EMAIL,
-   pass: process.env.WORD,
-   clientId: process.env.OAUTH_CLIENTID,
-   clientSecret: process.env.OAUTH_CLIENT_SECRET,
-   refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-   accessToken: accessToken
- },
- tls: {
-  rejectUnauthorized: false
-}
-}));
+let transporter = nodemailer.createTransport(
+	smtpTransport({
+		service: "gmail",
 
+		auth: {
+			type: "OAuth2",
+			user: process.env.EMAIL,
+			pass: process.env.WORD,
+			clientId: process.env.OAUTH_CLIENTID,
+			clientSecret: process.env.OAUTH_CLIENT_SECRET,
+			refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+			accessToken: accessToken,
+		},
+		tls: {
+			rejectUnauthorized: false,
+		},
+	})
+);
 
 transporter.verify((err, success) => {
- err
-   ? console.log(err)
-   : console.log(`=== Server is ready to take messages: ${success} ===`);
+	err
+		? console.log(err)
+		: console.log(`=== Server is ready to take messages: ${success} ===`);
 });
 
 app.post("/send", function (req, res) {
- let mailOptions = {
-   from: `${req.body.mailerState.email}`,
-   to: "mazurek321.dev@gmail.com",
-   subject: `Nowa wiadomość od ${req.body.mailerState.name} ( ${req.body.mailerState.email} ), temat: ${req.body.mailerState.subject}`,
-   text: `${req.body.mailerState.message}`,
- };
+	let mailOptions = {
+		from: `${req.body.mailerState.email}`,
+		to: "mazurek321.dev@gmail.com",
+		subject: `Nowa wiadomość od ${req.body.mailerState.name} ( ${req.body.mailerState.email} ), temat: ${req.body.mailerState.subject}`,
+		text: `${req.body.mailerState.message}`,
+	};
 
- transporter.sendMail(mailOptions, function (err, response) {
-   if (err) {
-     console.log(err);
-     res.json({
-       status: "fail",
-     });
-   } else {
-     console.log("== Message Sent ==");
-     res.json({
-       status: "success",
-     });
-   }
-   transporter.close();
- });
+	transporter.sendMail(mailOptions, function (err, response) {
+		if (err) {
+			console.log(err);
+			res.json({
+				status: "fail",
+			});
+		} else {
+			console.log("== Message Sent ==");
+			res.json({
+				status: "success",
+			});
+		}
+		transporter.close();
+	});
 });
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
- console.log(`Server is running on port: ${port}`);
+	console.log(`Server is running on port: ${port}`);
 });
